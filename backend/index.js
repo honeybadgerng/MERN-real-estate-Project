ce290d748203388d7501f7b09b67871554314705
honeybadger <rajimoshood200482@gmail.com>
2023-05-25 13:24:34 +0100

    created the authControllers

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const authController = require("./controllers/authController");
 const app = express();
 
 //Mongo DB connect
@@ -9,6 +10,10 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, () =>
   console.log("MongoDB sef don start")
 );
+//routes & middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/auth", authController);
 //connect server
-
 app.listen(process.env.PORT, () => console.log("Server don start"));
