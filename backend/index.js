d8fa443601ce5ad28914423fa49af852daf1b12f
honeybadger <rajimoshood200482@gmail.com>
2023-05-25 16:58:16 +0100

    created prop controller & corrected other errors

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const authController = require("./controllers/authController");
+const propertyController = require("./controllers/propertyController");
 const app = express();
 
 //Mongo DB connect
@@ -15,5 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authController);
+app.use("/property", propertyController);
 //connect server
 app.listen(process.env.PORT, () => console.log("Server don start"));
